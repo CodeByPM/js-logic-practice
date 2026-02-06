237ea6dcd42b81637f21f075f43aec84a465d69c
Priyanshu Maurya <priyanshu4490@outlook.com>
2026-02-06 15:39:32 +0530

    aj amine sikha switch case

--- if-else.js/switchCase.js
+++ if-else.js/switchCase.js
@@ -3,191 +3,191 @@
 
 //Q 1 exmple day print
 
-// let days = "wensaday"
+let days = "wensaday"
 
-// switch(days){
+switch(days){
  
-//     case "monday":
-//         console.log("stress for every employ");
-// break;
-// case "tuesday":
-//     console.log("work");
-// break;
-//     case "wensaday":
-//        console.log("todo projects to day");
-//        break;
-// case "sunday":
-//      console.log("work holiday");
-// break
-//      default:
-//          console.log("invalid day");
-// }
+    case "monday":
+        console.log("stress for every employ");
+break;
+case "tuesday":
+    console.log("work");
+break;
+    case "wensaday":
+       console.log("todo projects to day");
+       break;
+case "sunday":
+     console.log("work holiday");
+break
+     default:
+         console.log("invalid day");
+}
 
 //Q2️⃣ Traffic Signal
 
-// let signal ="red"
+let signal ="red"
 
-// switch(signal){
-//     case "green":
-//         console.log("go")
-//         break;
-//         case "red":
-//             console.log("stop")
-//             break;
-//             case "yellow":
-//                 console.log("ready")
-//                 break;
-//                 default:
-//                     console.log("invalid signal")
-// }
+switch(signal){
+    case "green":
+        console.log("go")
+        break;
+        case "red":
+            console.log("stop")
+            break;
+            case "yellow":
+                console.log("ready")
+                break;
+                default:
+                    console.log("invalid signal")
+}
 
 //Q3️⃣ Calculator (Basic)
-// let a=5
-// let b = 45
+let a=5
+let b = 45
 
-// let operator = "-"
+let operator = "-"
 
-// switch(operator){
-//     case "+":
-//         console.log(a+b);
-//         break;
-//         case "-":
-//         console.log(a-b);
-//         break;
-//         case "*":
-//         console.log(a*b);
-//         break;
-//         case "/":
-//         console.log(a/b);
-//         break;
-//         default:
-//         console.log("invalid operator");
+switch(operator){
+    case "+":
+        console.log(a+b);
+        break;
+        case "-":
+        console.log(a-b);
+        break;
+        case "*":
+        console.log(a*b);
+        break;
+        case "/":
+        console.log(a/b);
+        break;
+        default:
+        console.log("invalid operator");
         
-// }
+}
 
     //Q4️⃣ User Role
 
-    // let role = "user"
+    let role = "user"
 
-    // switch(role){
-    //     case "admin":
-    //     console.log("full acesss");
-    //     break;
-    //     case "editor":
-    //     console.log("editing acesss");
-    //     break;
-    //     case "user":
-    //     console.log("read acesss");
-    //     break;
-    //     case "other":
-    //     console.log("only viws acesss");
-    //     break;
-    //     default:
-    //         console.log("no acess")
+    switch(role){
+        case "admin":
+        console.log("full acesss");
+        break;
+        case "editor":
+        console.log("editing acesss");
+        break;
+        case "user":
+        console.log("read acesss");
+        break;
+        case "other":
+        console.log("only viws acesss");
+        break;
+        default:
+            console.log("no acess")
         
-    // }
+    }
 
-    //Q5️⃣ Browser Name
+  //  Q5️⃣ Browser Name
 
-    // let Browser = "safari";
-    // switch(Browser){
-    //     case "chrome":
-    //         console.log("google browser")
-    //         break;
-    //          case "firefox":
-    //         console.log("mozilla browser")
-    //         break;
-    //          case "safari":
-    //         console.log("apple browser")
-    //         break;
-    //         default:
-    //             console.log("unknown browser")
-    // }
+    let Browser = "safari";
+    switch(Browser){
+        case "chrome":
+            console.log("google browser")
+            break;
+             case "firefox":
+            console.log("mozilla browser")
+            break;
+             case "safari":
+            console.log("apple browser")
+            break;
+            default:
+                console.log("unknown browser")
+    }
 
     //Q6️⃣ ATM Menu
 
-//     let mychoice = 6;
-//     switch(mychoice){
-//         case 1:
-//             console.log("check balance")
-//             break;
-//                     case 2:
-//             console.log("money withdraw")
-//             break;
-//         case 3:
-//             console.log("deposit")
-//             break;
+    let mychoice = 6;
+    switch(mychoice){
+        case 1:
+            console.log("check balance")
+            break;
+                    case 2:
+            console.log("money withdraw")
+            break;
+        case 3:
+            console.log("deposit")
+            break;
             
-//                     case 4:
-//             console.log("exit")
-//             break;
-// default:
-//     console.log("invalid choice")
+                    case 4:
+            console.log("exit")
+            break;
+default:
+    console.log("invalid choice")
 
-//     }
+    }
 
 //Q7️⃣ Month Number → Season
 
 
-// let month = 19;
-// switch(month){
-//     case 12:
-//         console.log("winter")
-//         break;
-//           case 1:
-//         console.log("winter")
-//         break;
-//           case 2:
-//         console.log("winter")
-//         break;
-//           case 3:
-//         console.log("summer")
-//         break;
-//         case 4:
-//         console.log("summer")
-//         break;
-//         case 5:
-//         console.log("summer")
-//         break;
-//         case 6:
-//         console.log("rainy")
-//         break;
-//         case 7:
-//         console.log("rainy")
-//         break;
-//         case 8:
-//         console.log("rainy")
-//         break;
-//         case 9:
-//         console.log("anutumn")
-//         break;
-//         case 10:
-//         console.log("autumn")
-//         break;
-//         case 11:
-//         console.log("autumn")
-//         break;
-//         default:
-//             console.log("enter only 12 month")
-// }
+let month = 19;
+switch(month){
+    case 12:
+        console.log("winter")
+        break;
+          case 1:
+        console.log("winter")
+        break;
+          case 2:
+        console.log("winter")
+        break;
+          case 3:
+        console.log("summer")
+        break;
+        case 4:
+        console.log("summer")
+        break;
+        case 5:
+        console.log("summer")
+        break;
+        case 6:
+        console.log("rainy")
+        break;
+        case 7:
+        console.log("rainy")
+        break;
+        case 8:
+        console.log("rainy")
+        break;
+        case 9:
+        console.log("anutumn")
+        break;
+        case 10:
+        console.log("autumn")
+        break;
+        case 11:
+        console.log("autumn")
+        break;
+        default:
+            console.log("enter only 12 month")
+}
 
 //Q8️⃣ Game Difficulty
 
-// let level = "hard"
+let level = "hard"
 
-// switch(level){
-//     case "easy":
-//         console.log("3 lives")
-//         break;
-//          case "medium":
-//         console.log("2 lives")
-//         break;
-//          case "hard":
-//         console.log("1lives")
-//         break;
+switch(level){
+    case "easy":
+        console.log("3 lives")
+        break;
+         case "medium":
+        console.log("2 lives")
+        break;
+         case "hard":
+        console.log("1lives")
+        break;
 
-//         default:
-//             console.log("default level")
-// }
+        default:
+            console.log("default level")
+}
 
 
