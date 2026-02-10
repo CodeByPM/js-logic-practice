f79d5a77190a299fa9eeff97ccc8e1add0566ae9
Priyanshu Maurya <priyanshu4490@outlook.com>
2026-02-10 13:10:20 +0530

    maine small number nikalna sikha digit se aur number se

--- loopPractice/loop.js
+++ loopPractice/loop.js
@@ -87,3 +87,99 @@ else{
     console.log("no",reverse);
 }
 
+// -------------------------------------------------------------10 feb 2026------------------------------------
+// largest digit find in number;
+
+let smallest= 8639;
+ let max =8// hamne ek varible bana hai jo compare karega digit se jo number nikalega ;
+while(smallest>0){
+    let digit = smallest%10 // last number nikalne keliye
+    if(digit<max)
+      //// digit=max //// yaha maine galat assign kiya hu l
+    max=digit // ab ye sahi pahale digit me 0 assign kiya tha jise 0 print ho raha tha,
+      smallest =Math.floor(smallest/10)  // ye number ko remove karega
+}
+console.log(max)
+let largest= 8639;
+ let max =0// hamne ek varible bana hai jo compare karega digit se jo number nikalega ;
+while(largest>0){
+    let digit = largest%10 // last number nikalne keliye
+    if(digit>max)
+      //// digit=max //// yaha maine galat assign kiya hu 
+    max=digit // ab ye sahi pahale digit me 0 assign kiya tha jise 0 print ho raha tha,
+      largest =Math.floor(smallest/10)  // ye number ko remove karega
+}
+console.log(max)
+//                                     FOR LOOP + IF-ELSE (Logic Building)
+
+// q7 even number spratly prind karana hai 20 hai 10 even 10 odd
+
+let countEven=0
+let countOdd=0
+
+for(let i=1;i<=50;i++){
+    if(i%2==0){
+        countEven++
+    }else{
+        countOdd++
+    }
+}
+console.log("even count",countEven++)
+console.log("odd count",countOdd++)
+
+// q8 Print only numbers divisible by BOTH 3 AND 5 (1 to 50);
+for(let j=1;j<=50;j++){
+    if(j%3==0&&j%5==0){
+        console.log("divisible by 3 and 5",j)
+    }
+}
+
+// Q 9 FizzBuzz (Famous Interview Question!)
+1 to 30 tak:
+- Agar number 3 se divisible → "Fizz"
+- Agar number 5 se divisible → "Buzz"
+- Agar DONO se divisible → "FizzBuzz"
+- Baaki → number print karo
+
+Output:
+1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+...
+15 → FizzBuzz
+ for(let num =1 ; num<=30;num++){
+     if(num%3==0&&num%5==0){
+        console.log("fizzbuzz")
+    }
+     else if(num%3==0){
+        console.log("fizz")
+    }
+    else if(num%5==0){
+        console.log("buzz")
+    }
+    else{
+        console.log(num)
+    }
+ }
+//10 Find largest number in range (1 to 100 jo 7 se divide ho)
+let max =0;
+for(let i=0;i<=100;i++){
+    if(i%7==0&&i>max){
+        max=i
+    }
+}console.log(max)
+// Q11 Find smallest number in range (50 to 100 jo 9 se divide ho)
+
+let min =Infinity;
+for(let num = 50; num<=100;num++){
+    if(num%9==0&&num<min){
+min=num
+    }
+ 
+}console.log(min)
+
+// 12 Count how many numbers are divisible by 4 (1 to 100)
