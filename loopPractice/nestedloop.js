c48d45c24ae554b84705bd3d24e6af60d5686c71
Priyanshu Maurya <priyanshu4490@outlook.com>
2026-02-20 10:21:59 +0530

    learning pattern solve

--- loopPractice/nestedloop.js
+++ loopPractice/nestedloop.js
@@ -1,34 +1,61 @@
-// simple loop practice 
+// // simple loop practice 
 
-for(let num=1;num<=4;num++){
-    console.log("loop number",num)
-}
+// for(let num=1;num<=4;num++){
+//     console.log("loop number",num)
+// }
 
-// Example 1: The SIMPLEST Nested Loop
+// // Example 1: The SIMPLEST Nested Loop
 
-for(let classname =1;classname<=2;classname++){
-console.log("class name")
-   for(let student = 1; student<=3;student++){
-    console.log("students",student)
-   }
-console.log("........")
-}
+// for(let classname =1;classname<=2;classname++){
+// console.log("class name")
+//    for(let student = 1; student<=3;student++){
+//     console.log("students",student)
+//    }
+// console.log("........")
+// }
 
-// exmple no 2  adding star 
-row
-for(let row =1;row<=3;row++){//// pahale outerloop chalega 1 baar uske baad innerloop chalega 8 baar aise work karta hai loop
-    let line ="";// ye ham isliye hai varible hai yaha pe * store karenge
-    //column
-    for(let colun= 1;colun<=8;colun++){//// inner loop hai 
-        line+="*"
-    }  console.log(line)
-}
+// // exmple no 2  adding star 
+// row
+// for(let row =1;row<=3;row++){//// pahale outerloop chalega 1 baar uske baad innerloop chalega 8 baar aise work karta hai loop
+//     let line ="";// ye ham isliye hai varible hai yaha pe * store karenge
+//     //column
+//     for(let colun= 1;colun<=8;colun++){//// inner loop hai 
+//         line+="*"
+//     }  console.log(line)
+// }
 
-// Exercise 1: Number Grid
-for(let row = 1; row<=3;row++){/// outer loop hoti ye 
-    console.log("row",row)
-    for(let colun= 1; colun<=3;colun++){
-        console.log("hello")
-    }
-}
+// // Exercise 1: Number Grid
+// for(let row = 1; row<=3;row++){/// outer loop hoti ye 
+//     console.log("row",row)
+//     for(let colun= 1; colun<=3;colun++){
+//         console.log("hello")
+//     }
+// }
+/// 13 feb 2026
+
+// excersise 2 : print 111,222,333
+// let sum =0;//Jab bhi continuously increasing values chahiye:
+// for(let num=1; num<=3;num++){
+//     let lines=""
+//     for(let j =1; j<=3;j++){
+//        lines+=num /// rule jab bhi row me same value chahiye to add kardo  ye num assign
+//            console.log(num) 
+//     }
+//    console.log(".....")
+// }
+
+//// excersise 3 : print 555 3 times 
+
+// let lines =""// yaha pe jo varible liya hai to answer 555 pyramid me aa raha tha 
+// for(let num =1;num<=3;num++){
+//     let lines =""//jab maine loop ke andar liya ye varible to answer sahi aa raha tha /but kyu aisa different hai explain karo
+//     for(let j=1;j<=3;j++){
+//         lines+="5"
+       
+//     }
+//  console.log(lines)
+// }
+
+
+
 
