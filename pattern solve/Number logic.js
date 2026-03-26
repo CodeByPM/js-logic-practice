8d30ff299729358515dc9d8b56661dcd1ee22459
Priyanshu Maurya <priyanshu4490@outlook.com>
2026-03-26 14:39:53 +0530

    aj maine table print kiya

---
+++ pattern solve/Number logic.js
@@ -0,0 +1,13 @@
+//1. Multiplication Table 1 to 5
+// 1 2 3 4 5
+// 2 4 6 8 10
+// 3 6 9 12 15
+// 4 8 12 16 20
+// 5 10 15 20 25
+for(let i=1;i<=5;i++){
+    let digit="";
+    for(let j=1;j<=5;j++){
+        digit+=(i*j)+" "
+    }
+    console.log(digit)
+}
